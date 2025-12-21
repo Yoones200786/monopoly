000ee0aeeddaf1d6b3d7cac8415c7d3e7c68156b
AmirFomic <amirfomic@gmail.com>
2025-12-22 01:32:19 +0330

    Adding sign-up func update

--- sign-up.py
+++ sign-up.py
@@ -1,6 +1,7 @@
 import re
 import uuid
-import 
+import bcrypt
+import json
 def mainmenu():
     print ("\033[1;32mWelcome to Monopoly!\033[0m\n")
     print ("1. New Game")
@@ -24,23 +25,74 @@ def signupmenu():
     print ("1. Signup")
     print ("2. Login")
     print ("3. Exit")
-def emailisvalid(x):
-
-
-
-
-
-
-
-
-
-
-
+    choice = input ("Enter your choice (1-4):")
+    if choice == "1":
+        username = input("Enter a username:")
+        while usenameisnew(username) == False:
+            print ("username is already taken, please try again!")
+            username = input("Enter a username:")
+        email = input("Enter an email:")
+        while emailisvalid(email) == False or emailisnew(email)== False:
+            print ("Please try again!")
+            email = input("Enter an email:")
+        password = input("Enter a strong password!:")
+        while passisvalid(password) == False:
+            print ("please try again!")
+            password  = input("Enter a strong password!:")
+        user_id = str(uuid.uuid4())
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt()).decode("utf-8")
+        user_data = {
+            user_id: {
+                "username": username,
+                "email": email,
+                "password": hashed_password
+            }
+        }
+        try:
+            with open("sign-up.json", "r") as f:
+                users = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            users = {}
+        users.update(user_data)
+        with open ("sign-up.json", "w") as f:
+            json.dump(users, f, indent=4)
+def emailisvalid(email):
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(email_pattern, email):
+        return True
+    else:
+        return False
+def passisvalid(password):
+    if len(password) > 8:
+        return True
+    else:
+        return False
+def usenameisnew(username):
+    try:
+        with open("sign-up.json", "r") as f:
+            users = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        users = {}
+    for u in users.values():
+        if u.get("username") == username:
+            return False
+    return True
+def emailisnew(email):
+    try:
+        with open("sign-up.json", "r") as f:
+            users = json.load(f)
+    except (FileNotFoundError,json.JSONDecodeError):
+        users = {}
+    for u in users.values():
+        if u.get("email") == email:
+            return False
+    return True    
 def loadgame():
     print ("The Functuion is not ready yet!")
 def leaderboard():
     print ("The Functuion is not ready yet!")
 mainmenu()
+
 
 
 
