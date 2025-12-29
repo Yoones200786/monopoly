d179dcdfbbbb7124667790ef9aa4b1dca28c6713
mahdir161 <mahdirostami12@gmail.com>
2025-12-29 14:10:22 +0330

    Add files via upload
    added mortgage

--- board_setup.py
+++ board_setup.py
@@ -4,6 +4,7 @@ monopoly_data = {
         "type": "special",
         "action": "collect",
         "amount": 200
+        ,"owner":"gov"
     },
     "2": {
         "name": "Mediterranean Avenue",
@@ -13,10 +14,13 @@ monopoly_data = {
         "rent": [2, 10, 30, 90, 160, 250],
         "house_cost": 50,
         "hotel_cost": 50
+        ,"owner":""
     },
     "3": {
         "name": "Community Chest 1",
         "type": "community_chest"
+        , "owner": "gov"
+
     },
     "4": {
         "name": "Baltic Avenue",
@@ -25,18 +29,24 @@ monopoly_data = {
         "buy_price": 60,
         "rent": [4, 20, 60, 180, 320, 450],
         "house_cost": 50,
-        "hotel_cost": 50
+        "hotel_cost": 50,
+        "owner":""
+
     },
     "5": {
         "name": "Income Tax",
         "type": "tax",
         "amount": 200
+        , "owner": "gov"
+
     },
     "6": {
         "name": "Reading Railroad",
         "type": "railroad",
         "buy_price": 200,
         "rent": [25, 50, 100, 200]
+        , "owner": ""
+
     },
     "7": {
         "name": "Oriental Avenue",
@@ -46,10 +56,13 @@ monopoly_data = {
         "rent": [6, 30, 90, 270, 400, 550],
         "house_cost": 50,
         "hotel_cost": 50
-    },
+        , "owner": ""
+},
     "8": {
         "name": "Chance 1",
         "type": "chance"
+        , "owner": "gov"
+
     },
     "9": {
         "name": "Vermont Avenue",
@@ -59,7 +72,9 @@ monopoly_data = {
         "rent": [6, 30, 90, 270, 400, 550],
         "house_cost": 50,
         "hotel_cost": 50
+        , "owner": ""
     },
+
     "10": {
         "name": "Connecticut Avenue",
         "type": "property",
@@ -68,11 +83,13 @@ monopoly_data = {
         "rent": [8, 40, 100, 300, 450, 600],
         "house_cost": 50,
         "hotel_cost": 50
-    },
+        , "owner": ""},
     "11": {
         "name": "Jail",
         "type": "special",
         "action": "just_visiting"
+        , "owner": "gov"
+
     },
     "12": {
         "name": "St. Charles Place",
@@ -82,12 +99,16 @@ monopoly_data = {
         "rent": [10, 50, 150, 450, 625, 750],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "13": {
         "name": "Electric Company",
         "type": "utility",
         "buy_price": 150,
         "rent_multiplier": [4, 10]
+        , "owner": ""
+
     },
     "14": {
         "name": "States Avenue",
@@ -97,6 +118,8 @@ monopoly_data = {
         "rent": [10, 50, 150, 450, 625, 750],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "15": {
         "name": "Virginia Avenue",
@@ -106,12 +129,16 @@ monopoly_data = {
         "rent": [12, 60, 180, 500, 700, 900],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "16": {
         "name": "Pennsylvania Railroad",
         "type": "railroad",
         "buy_price": 200,
         "rent": [25, 50, 100, 200]
+        , "owner": ""
+
     },
     "17": {
         "name": "St. James Place",
@@ -121,10 +148,14 @@ monopoly_data = {
         "rent": [14, 70, 200, 550, 750, 950],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "18": {
         "name": "Community Chest 2",
         "type": "community_chest"
+        , "owner": "gov"
+
     },
     "19": {
         "name": "Tennessee Avenue",
@@ -134,6 +165,8 @@ monopoly_data = {
         "rent": [14, 70, 200, 550, 750, 950],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "20": {
         "name": "New York Avenue",
@@ -143,10 +176,14 @@ monopoly_data = {
         "rent": [16, 80, 220, 600, 800, 1000],
         "house_cost": 100,
         "hotel_cost": 100
+        , "owner": ""
+
     },
     "21": {
         "name": "Free Parking",
         "type": "special"
+        , "owner": "gov"
+
     },
     "22": {
         "name": "Kentucky Avenue",
@@ -156,10 +193,14 @@ monopoly_data = {
         "rent": [18, 90, 250, 700, 875, 1050],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "23": {
         "name": "Chance 2",
         "type": "chance"
+        , "owner": "gov"
+
     },
     "24": {
         "name": "Indiana Avenue",
@@ -168,6 +209,8 @@ monopoly_data = {
         "buy_price": 220,"rent": [18, 90, 250, 700, 875, 1050],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "25": {
         "name": "Illinois Avenue",
@@ -177,12 +220,16 @@ monopoly_data = {
         "rent": [20, 100, 300, 750, 925, 1100],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "26": {
         "name": "B&O Railroad",
         "type": "railroad",
         "buy_price": 200,
         "rent": [25, 50, 100, 200]
+        , "owner": ""
+
     },
     "27": {
         "name": "Atlantic Avenue",
@@ -192,6 +239,8 @@ monopoly_data = {
         "rent": [22, 110, 330, 800, 975, 1150],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "28": {
         "name": "Ventnor Avenue",
@@ -201,12 +250,16 @@ monopoly_data = {
         "rent": [22, 110, 330, 800, 975, 1150],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "29": {
         "name": "Water Works",
         "type": "utility",
         "buy_price": 150,
         "rent_multiplier": [4, 10]
+        , "owner": ""
+
     },
     "30": {
         "name": "Marvin Gardens",
@@ -216,11 +269,15 @@ monopoly_data = {
         "rent": [24, 120, 360, 850, 1025, 1200],
         "house_cost": 150,
         "hotel_cost": 150
+        , "owner": ""
+
     },
     "31": {
         "name": "Go to Jail",
         "type": "special",
         "action": "goto_jail"
+        , "owner": "gov"
+
     },
     "32": {
         "name": "Pacific Avenue",
@@ -230,6 +287,8 @@ monopoly_data = {
         "rent": [26, 130, 390, 900, 1100, 1275],
         "house_cost": 200,
         "hotel_cost": 200
+        , "owner": ""
+
     },
     "33": {
         "name": "North Carolina Avenue",
@@ -239,10 +298,14 @@ monopoly_data = {
         "rent": [26, 130, 390, 900, 1100, 1275],
         "house_cost": 200,
         "hotel_cost": 200
+        , "owner": ""
+
     },
     "34": {
         "name": "Community Chest 3",
         "type": "community_chest"
+        , "owner": "gov"
+
     },
     "35": {
         "name": "Pennsylvania Avenue",
@@ -252,16 +315,22 @@ monopoly_data = {
         "rent": [28, 150, 450, 1000, 1200, 1400],
         "house_cost": 200,
         "hotel_cost": 200
+        , "owner": ""
+
     },
     "36": {
         "name": "Short Line",
         "type": "railroad",
         "buy_price": 200,
         "rent": [25, 50, 100, 200]
+        , "owner": ""
+
     },
     "37": {
         "name": "Chance 3",
         "type": "chance"
+        , "owner": "gov"
+
     },
     "38": {
         "name": "Park Place",
@@ -271,11 +340,15 @@ monopoly_data = {
         "rent": [35, 175, 500, 1100, 1300, 1500],
         "house_cost": 200,
         "hotel_cost": 200
+        , "owner": ""
+
     },
     "39": {
         "name": "Luxury Tax",
         "type": "tax",
         "amount": 100
+        , "owner": "gov"
+
     },
     "40": {
         "name": "Boardwalk",
@@ -285,12 +358,14 @@ monopoly_data = {
         "rent": [50, 200, 600, 1400, 1700, 2000],
         "house_cost": 200,
         "hotel_cost": 200
+        , "owner": ""
+
     }
 }
 board = [None] * 40
 for key in monopoly_data:
-    pos = int(key) - 1  
-    board[pos] = monopoly_data[key].copy() 
+    pos = int(key) - 1
+    board[pos] = monopoly_data[key].copy()
 def initialize_board():
     for i in range(40):
         space = board[i]
@@ -298,7 +373,7 @@ def initialize_board():
             space["owner"] = None
             space["mortgaged"] = False
             if space["type"] == "property":
-                space["houses"] = 0 
+                space["houses"] = 0
 color_groups = {
     "brown": [2, 4],
     "light_blue": [7, 9, 10],
